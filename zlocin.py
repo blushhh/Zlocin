fc5f9cfa94f173546eeaf1eacd8b6bb04a3dae13
Blaž Dvorjak <blaz.dvo@gmail.com>
2016-03-19 14:36:33 +0100

    urejena koda

--- zlocin.py
+++ zlocin.py
@@ -25,14 +25,26 @@ miha_oblika_obraza = "GCCACGG"
 
 match = "1 match found: "
 
-if (dnk.find(ziga_spol)>0) and (dnk.find(ziga_rasa)>0) and (dnk.find(ziga_barva_las)>0) and (dnk.find(ziga_barva_oci)>0) and (dnk.find(ziga_oblika_obraza)>0):
-    print match + "Sladoled je pojedel Žiga."
+if (dnk.find(ziga_spol)>0) and \
+   (dnk.find(ziga_rasa)>0) and \
+   (dnk.find(ziga_barva_las)>0) and \
+   (dnk.find(ziga_barva_oci)>0) and \
+   (dnk.find(ziga_oblika_obraza)>0):
+   print match + "Sladoled je pojedel Žiga."
 
-if (dnk.find(matej_spol)>0) and (dnk.find(matej_rasa)>0) and (dnk.find(matej_barva_las)>0) and (dnk.find(matej_barva_oci)>0) and (dnk.find(matej_oblika_obraza)>0):
-    print match + "Sladoled je pojedel Matej."
+if (dnk.find(matej_spol)>0) and \
+   (dnk.find(matej_rasa)>0) and \
+   (dnk.find(matej_barva_las)>0) and \
+   (dnk.find(matej_barva_oci)>0) and \
+   (dnk.find(matej_oblika_obraza)>0):
+   print match + "Sladoled je pojedel Matej."
 
-if (dnk.find(miha_spol)>0) and (dnk.find(miha_rasa)>0) and (dnk.find(miha_barva_las)>0) and (dnk.find(miha_barva_oci)>0) and (dnk.find(miha_oblika_obraza)>0):
-    print match + "Sladoled je pojedel Miha."
+if (dnk.find(miha_spol)>0) and \
+   (dnk.find(miha_rasa)>0) and \
+   (dnk.find(miha_barva_las)>0) and \
+   (dnk.find(miha_barva_oci)>0) and \
+   (dnk.find(miha_oblika_obraza)>0):
+   print match + "Sladoled je pojedel Miha."
 
 
 
